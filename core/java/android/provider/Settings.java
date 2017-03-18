5d8a9aec16a5d38e0306ce21318f8a02a28d2020
Pranav Vashi <neobuddy89@gmail.com>
2017-03-19 01:48:25 +0530

    SystemUI: Add ability to toggle bluetooth battery level
    [xdroidOSS]
    - Show bluetooth battery level
    - Squash toggle in tuner prefs
    - Add icon
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: jrjmt <juliusronald12@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5610,6 +5610,12 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_LOCKSCREEN = "double_tap_sleep_lockscreen";
 
         /**
+         * Enable/disable Bluetooth Battery bar
+         * @hide
+         */
+        public static final String BLUETOOTH_SHOW_BATTERY = "bluetooth_show_battery";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
