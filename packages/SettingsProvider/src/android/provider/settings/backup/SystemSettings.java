4d3acd709b07a99e88343a69985cf2f062932390
xyyx <xyyx@mail.ru>
2018-08-16 06:27:04 +0000

    [SQUASH] base: Lockscreen Charging (1/3)
    needed system/core patch
    based on:
    [2/2] frameworks: show charging current & voltage on lockscreen by yank555-lu
    [2/2]Base: show more battery info on lockscreen when charging by yank555-lu
    LockscreenCharging: Use double for mChargingVoltage and show as4.XV by xyyx
    Lockscreen charging: Formatting improvements by nathanchance
    
    DennySPb: adapt to A11
    @/idoybh: Add settings value to backup array
    [jhonboy121]: adapt to A12 and improve code
        * added a settings observer to update a boolean mShowBatteryInfo, instead of refetching it everytime
          power indication is updated
        * update indication text on settings change
    
    Change-Id: I42f90b413fd1445ded00cb5769af0ce5faf15c49
    
    Co-authored-by: yank555-lu <yank555.lu@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -92,5 +92,6 @@ public class SystemSettings {
         //xdroidOSS additions
         Settings.System.NETWORK_TRAFFIC_STATE,
         Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD,
+        Settings.System.LOCKSCREEN_BATTERY_INFO,
     };
 }
