5b1b872d26896356ce9a1cead140ea975511c5a2
LuK1337 <priv.luk@gmail.com>
2021-10-09 21:28:49 +0200

    Fallback to global actions if assistant is not available
    Change-Id: I8c5c34f8b29967eace022ab3eed035b1e68efcb6

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -626,6 +626,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     private AssistUtils mAssistUtils;
 
+    private AssistUtils mAssistUtils;
+
     // Maps global key codes to the components that will handle them.
     private GlobalKeyManager mGlobalKeyManager;
 
