cf74c3331eb8b7c43536b0c0706d584c5fd65475
ReallySnow <reallysnow233@gmail.com>
2022-07-08 17:36:35 +0800

    base: Import Wi-Fi standard icon
    * Show Wi-Fi 4/5/6 standard on statusbar
    idoybh: added setting to backup
    
    [PixelOS]
      - Squash toggle in tuner prefs
      - Minor string edits and drop summary
    
    Co-authored-by: Col_or <col_or@qq.com>
    Co-authored-by: Kujou Yuko <i@779.moe>
    Change-Id: I3658cda5b303bd03cf83e174e4dfd6a55d33e860
    Signed-off-by: Karan Parashar <karan@pixelos.net>
    Signed-off-by: basamaryan <aryan@pixelos.net>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11241,6 +11241,12 @@ public final class Settings {
          * @hide
          */
         public static final String LOCK_SCREEN_WEATHER_ENABLED = "lockscreen_weather_enabled";
+        
+        /**
+         * Whether tod show Wi-Fi standard icon
+         * @hide
+         */
+        public static final String SHOW_WIFI_STANDARD_ICON = "show_wifi_standard_icon";
 
         /**
          * These entries are considered common between the personal and the managed profile,
