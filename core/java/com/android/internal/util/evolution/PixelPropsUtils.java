2c1241ffcb641e604540f696e3b7a3f7b9a067e4
Luke Williams <luke@expressluke.com>
2023-03-02 19:07:26 +0000

    PixelPropsUtils: Spoof Samsung Accessory apps
    Some Samsung wearable plugins such as Galaxy Buds2 Pro Manager use a different package name to other Samsung apps and hence do not function without spoofing first.
    
    Spoof com.samsung.accessory.*  in addition to com.samsung.android*

--- core/java/com/android/internal/util/evolution/PixelPropsUtils.java
+++ core/java/com/android/internal/util/evolution/PixelPropsUtils.java
@@ -44,6 +44,7 @@ public class PixelPropsUtils {
     private static final String PACKAGE_PS = "com.android.vending";
     private static final String PACKAGE_SI = "com.google.android.settings.intelligence";
     private static final String SAMSUNG = "com.samsung.android.";
+    private static final String SAMSUNGACCESSORY = "com.samsung.accessory.";
     private static final String SPOOF_MUSIC_APPS = "persist.sys.disguise_props_for_music_app";
 
     private static final String sCertifiedFp =
@@ -261,6 +262,7 @@ public class PixelPropsUtils {
         }
         if (packageName.startsWith("com.google.")
                 || packageName.startsWith(SAMSUNG)
+                || packageName.startsWith(SAMSUNGACCESSORY)
                 || Arrays.asList(extraPackagesToChange).contains(packageName)) {
 
             Map<String, Object> propsToChange = new HashMap<>();
