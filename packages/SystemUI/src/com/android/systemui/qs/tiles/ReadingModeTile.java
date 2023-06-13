PixelOS-AOSP/frameworks_base

SystemUI: Add reading mode tile
Author: Joey <joey@lineageos.org>
Date:   Wed May 16 17:16:06 2018 +0200

    base: add reading mode tile

    Change-Id: I1c515cbeff0dd5a1fa0c00bc9390a66eb104c7f0
    Signed-off-by: Joey <joey@lineageos.org>

Author: Michael Bestas <mkbestas@lineageos.org>
Date:   Sat May 26 00:58:57 2018 +0300

    SystemUI: Fix reading mode tile accessibility strings

    Change-Id: I4f0af56e4947b037e1e2dda79132523d13458818

Author: Paul Keith <javelinanddart@gmail.com>
Date:   Wed Aug 8 18:59:25 2018 +0200

    SystemUI: Update reading mode tile for new API

    Change-Id: I9fbf1b4de30079c987b75e37e52e234fa2f86c5d

Change-Id: I365ee54b2b2adc52d804a5fc1d35690ef5636071

commit 3dce873061757be3ff97dd4d48f90f71f1e8e004
Author: Bruno Martins <bgcngm@gmail.com>
Date:   Sat Apr 11 14:09:58 2020 +0100

    ReadingModeTile: Go away if wellbeing app is enabled

    Google's wellbeing app provides Wind Down feature along with
    a grayscale option and a tile for it. Don't show up Reading Mode
    tile to avoid having duplicate features.

    Change-Id: I4735b57253a787c098984f456ce4da40dbd17c94

commit a3246ce92dd7d8b401a96bf1d2241c3b05e61efe
Author: Bruno Martins <bgcngm@gmail.com>
Date:   Fri May 14 23:06:29 2021 +0100

    ReadingModeTile: Drop dependency on wellbeing app

    Google's wellbeing app used to provide Wind Down feature along with
    a grayscale option and a tile for it. It is no longer the case,
    so bring back our tile unconditionally.

    Change-Id: I5a29d4f8f86dfe0361064842da8c9a3b2ce33d47

Change-Id: Iffc81d37452cc6989f19715ab8508c517a7fcd83

packages/SystemUI/src/com/android/systemui/qs/tiles/ReadingModeTile.java
@@ -1,0 +1,121 @@
+/*
+ * Copyright (C) 2018-2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
+import android.service.quicksettings.Tile;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+import com.android.internal.lineage.hardware.LineageHardwareManager;
+import com.android.internal.logging.MetricsLogger;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.systemui.R;
+import com.android.systemui.dagger.qualifiers.Background;
+import com.android.systemui.dagger.qualifiers.Main;
+import com.android.systemui.plugins.ActivityStarter;
+import com.android.systemui.plugins.FalsingManager;
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.plugins.statusbar.StatusBarStateController;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.qs.logging.QSLogger;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+
+import javax.inject.Inject;
+
+public class ReadingModeTile extends QSTileImpl<BooleanState> {
+
+    private final Icon mIcon = ResourceIcon.get(R.drawable.ic_qs_reader);
+
+    private static final Intent DISPLAY_SETTINGS = new Intent("android.settings.DISPLAY_SETTINGS");
+
+    private LineageHardwareManager mHardware;
+
+    @Inject
+    public ReadingModeTile(
+            QSHost host,
+            @Background Looper backgroundLooper,
+            @Main Handler mainHandler,
+            FalsingManager falsingManager,
+            MetricsLogger metricsLogger,
+            StatusBarStateController statusBarStateController,
+            ActivityStarter activityStarter,
+            QSLogger qsLogger) {
+        super(host, backgroundLooper, mainHandler, falsingManager, metricsLogger,
+                statusBarStateController, activityStarter, qsLogger);
+        mHardware = LineageHardwareManager.getInstance(mContext);
+    }
+
+    @Override
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    protected void handleClick(@Nullable View view) {
+        boolean newStatus = !isReadingModeEnabled();
+        mHardware.set(LineageHardwareManager.FEATURE_READING_ENHANCEMENT, newStatus);
+        refreshState();
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return DISPLAY_SETTINGS;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return mHardware.isSupported(LineageHardwareManager.FEATURE_READING_ENHANCEMENT);
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        state.value = isReadingModeEnabled();
+        state.icon = mIcon;
+        if (state.value) {
+            state.contentDescription = mContext.getString(
+                    R.string.accessibility_quick_settings_reading_mode_on);
+            state.state = Tile.STATE_ACTIVE;
+        } else {
+            state.contentDescription = mContext.getString(
+                    R.string.accessibility_quick_settings_reading_mode_off);
+            state.state = Tile.STATE_INACTIVE;
+        }
+        state.label = getTileLabel();
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.quick_settings_reading_mode);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.CUSTOM;
+    }
+
+    @Override
+    public void handleSetListening(boolean listening) {
+    }
+
+    private boolean isReadingModeEnabled() {
+        return mHardware.get(LineageHardwareManager.FEATURE_READING_ENHANCEMENT);
+    }
+}

xDroidOSS-Pixel/platform_frameworks_base

packages/SystemUI/src/com/android/systemui/qs/tiles/ReadingModeTile.java
@@ -1,0 +1,121 @@
+/*
+ * Copyright (C) 2018-2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
+import android.service.quicksettings.Tile;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+import com.android.internal.lineage.hardware.LineageHardwareManager;
+import com.android.internal.logging.MetricsLogger;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.systemui.R;
+import com.android.systemui.dagger.qualifiers.Background;
+import com.android.systemui.dagger.qualifiers.Main;
+import com.android.systemui.plugins.ActivityStarter;
+import com.android.systemui.plugins.FalsingManager;
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.plugins.statusbar.StatusBarStateController;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.qs.logging.QSLogger;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+
+import javax.inject.Inject;
+
+public class ReadingModeTile extends QSTileImpl<BooleanState> {
+
+    private final Icon mIcon = ResourceIcon.get(R.drawable.ic_qs_reader);
+
+    private static final Intent DISPLAY_SETTINGS = new Intent("android.settings.DISPLAY_SETTINGS");
+
+    private LineageHardwareManager mHardware;
+
+    @Inject
+    public ReadingModeTile(
+            QSHost host,
+            @Background Looper backgroundLooper,
+            @Main Handler mainHandler,
+            FalsingManager falsingManager,
+            MetricsLogger metricsLogger,
+            StatusBarStateController statusBarStateController,
+            ActivityStarter activityStarter,
+            QSLogger qsLogger) {
+        super(host, backgroundLooper, mainHandler, falsingManager, metricsLogger,
+                statusBarStateController, activityStarter, qsLogger);
+        mHardware = LineageHardwareManager.getInstance(mContext);
+    }
+
+    @Override
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    protected void handleClick(@Nullable View view) {
+        boolean newStatus = !isReadingModeEnabled();
+        mHardware.set(LineageHardwareManager.FEATURE_READING_ENHANCEMENT, newStatus);
+        refreshState();
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return DISPLAY_SETTINGS;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return mHardware.isSupported(LineageHardwareManager.FEATURE_READING_ENHANCEMENT);
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        state.value = isReadingModeEnabled();
+        state.icon = mIcon;
+        if (state.value) {
+            state.contentDescription = mContext.getString(
+                    R.string.accessibility_quick_settings_reading_mode_on);
+            state.state = Tile.STATE_ACTIVE;
+        } else {
+            state.contentDescription = mContext.getString(
+                    R.string.accessibility_quick_settings_reading_mode_off);
+            state.state = Tile.STATE_INACTIVE;
+        }
+        state.label = getTileLabel();
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.quick_settings_reading_mode);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.XD_SETTINGS;
+    }
+
+    @Override
+    public void handleSetListening(boolean listening) {
+    }
+
+    private boolean isReadingModeEnabled() {
+        return mHardware.get(LineageHardwareManager.FEATURE_READING_ENHANCEMENT);
+    }
+}