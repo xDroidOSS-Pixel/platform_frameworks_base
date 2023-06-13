PixelOS-AOSP/frameworks_base

SystemUI: Add LiveDisplay tile
Author: Steve Kondik <steve@cyngn.com>
Date:   Sun Nov 5 16:37:52 2017 +0100

    SystemUI: LiveDisplay tile

    Change-Id: I53d492e5cb9998268104d5750705aa5ed55d9658
    Signed-off-by: Joey Rizzoli <joey@lineageos.org>

Author: Unpublished <unpublished@gmx.net>
Date:   Fri Dec 29 23:00:01 2017 +0100

    livedisplay: Allow tile creation before boot completed phase [2/2]

    Change-Id: Ibef44d4e07da9baf296796515288c9d42aba8608

Author: Michael Bestas <mkbestas@lineageos.org>
Date:   Thu Feb 1 16:39:16 2018 +0000
Edit:   Refactor for Oreo

Change-Id: Ibef44d4e07da9baf296796515288c9d42aba8608

Author: Bruno Martins <bgcngm@gmail.com>
Date:   Thu Feb 1 17:01:43 2018 +0000

    LiveDisplayTile: Avoid NPE during boot up phase

     * During boot, with LineageHW not yet initialized there is no way to
       check the current LiveDisplay mode. In such cases, fallback to AUTO.

    Change-Id: Ifc004c86dfd68f73b8a362a1c833cbc3e7397811

Author: Han Wang <416810799@qq.com>
Date:   Tue Jun 4 18:03:59 2019 +0200

    LiveDisplayTile: Report unavailable on HWC2

    Change-Id: Ic10381e25046a2338bc6bda36ecbb5d5cefc1ab8

commit 9d95e3e77af723c6c783ee528d8d0a6f96369af5
Author: jhenrique09 <jhenrique09.mcz@hotmail.com>
Date:   Mon Aug 26 14:21:17 2019 -0300

    QSTileHost: Recreate tiles when LiveDisplay gets initialized

    Revert "LiveDisplayTile: Refresh state after livedisplay initialization"

    This reverts commit 42e327581e1006c30339c886ea381c162e3f5299.

    Change-Id: I9976076593684418c318e66803cf6e4f450975b2

commit 6b67f3b98fb5b5db4daf44f43afb6323f5bed57c
Author: DennySPB <dennyspb@gmail.com>
Date:   Mon Sep 23 16:52:22 2019 +0300

    LiveDisplayTile: Fixup modes cycling for aosp implementation

    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: I5edcef553ae32b4eb72882ef81ffe56f5074fc0d

commit 583bb96890ca118862513aa5bad9f71a97d4973b
Author: LuK1337 <priv.luk@gmail.com>
Date:   Fri Sep 6 23:41:14 2019 +0200

    LiveDisplay

packages/SystemUI/src/com/android/systemui/qs/tiles/LiveDisplayTile.java
@@ -1,0 +1,251 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ * Copyright (C) 2018-2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import static com.android.internal.lineage.hardware.LiveDisplayManager.FEATURE_MANAGED_OUTDOOR_MODE;
+import static com.android.internal.lineage.hardware.LiveDisplayManager.MODE_AUTO;
+import static com.android.internal.lineage.hardware.LiveDisplayManager.MODE_DAY;
+import static com.android.internal.lineage.hardware.LiveDisplayManager.MODE_NIGHT;
+import static com.android.internal.lineage.hardware.LiveDisplayManager.MODE_OFF;
+import static com.android.internal.lineage.hardware.LiveDisplayManager.MODE_OUTDOOR;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.database.ContentObserver;
+import android.hardware.display.ColorDisplayManager;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.service.quicksettings.Tile;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+import com.android.internal.lineage.hardware.LiveDisplayManager;
+import com.android.internal.logging.MetricsLogger;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.util.ArrayUtils;
+import com.android.systemui.dagger.qualifiers.Background;
+import com.android.systemui.dagger.qualifiers.Main;
+import com.android.systemui.plugins.ActivityStarter;
+import com.android.systemui.plugins.FalsingManager;
+import com.android.systemui.plugins.qs.QSTile.LiveDisplayState;
+import com.android.systemui.plugins.statusbar.StatusBarStateController;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.qs.logging.QSLogger;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+
+import javax.inject.Inject;
+
+import com.android.internal.R;
+
+/**
+ * Quick settings tile: LiveDisplay mode switcher
+ **/
+public class LiveDisplayTile extends QSTileImpl<LiveDisplayState> {
+
+    private static final Intent DISPLAY_SETTINGS = new Intent("android.settings.DISPLAY_SETTINGS");
+
+    private final LiveDisplayObserver mObserver;
+    private String mTitle;
+    private String[] mEntries;
+    private String[] mDescriptionEntries;
+    private String[] mAnnouncementEntries;
+    private String[] mValues;
+    private final int[] mEntryIconRes;
+
+    private boolean mListening;
+
+    private int mDayTemperature;
+
+    private final boolean mNightDisplayAvailable;
+    private final boolean mOutdoorModeAvailable;
+
+    private final LiveDisplayManager mLiveDisplay;
+
+    private static final int OFF_TEMPERATURE = 6500;
+
+    @Inject
+    public LiveDisplayTile(
+            QSHost host,
+            @Background Looper backgroundLooper,
+            @Main Handler mainHandler,
+            FalsingManager falsingManager,
+            MetricsLogger metricsLogger,
+            StatusBarStateController statusBarStateController,
+            ActivityStarter activityStarter,
+            QSLogger qsLogger) {
+        super(host, backgroundLooper, mainHandler, falsingManager, metricsLogger,
+                statusBarStateController, activityStarter, qsLogger);
+        mNightDisplayAvailable = ColorDisplayManager.isNightDisplayAvailable(mContext);
+
+        Resources res = mContext.getResources();
+        TypedArray typedArray = res.obtainTypedArray(R.array.live_display_drawables);
+        mEntryIconRes = new int[typedArray.length()];
+        for (int i = 0; i < mEntryIconRes.length; i++) {
+            mEntryIconRes[i] = typedArray.getResourceId(i, 0);
+        }
+        typedArray.recycle();
+
+        updateEntries();
+
+        mLiveDisplay = LiveDisplayManager.getInstance(mContext);
+        if (mLiveDisplay.getConfig() != null) {
+            mOutdoorModeAvailable = mLiveDisplay.getConfig().hasFeature(MODE_OUTDOOR) &&
+                    !mLiveDisplay.getConfig().hasFeature(FEATURE_MANAGED_OUTDOOR_MODE);
+            mDayTemperature = mLiveDisplay.getDayColorTemperature();
+        } else {
+            mOutdoorModeAvailable = false;
+            mDayTemperature = -1;
+        }
+
+        mObserver = new LiveDisplayObserver(mHandler);
+        mObserver.startObserving();
+    }
+
+    private void updateEntries() {
+        Resources res = mContext.getResources();
+        mTitle = res.getString(R.string.live_display_title);
+        mEntries = res.getStringArray(R.array.live_display_entries);
+        mDescriptionEntries = res.getStringArray(R.array.live_display_description);
+        mAnnouncementEntries = res.getStringArray(R.array.live_display_announcement);
+        mValues = res.getStringArray(R.array.live_display_values);
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return !mNightDisplayAvailable || mOutdoorModeAvailable;
+    }
+
+    @Override
+    public LiveDisplayState newTileState() {
+        return new LiveDisplayState();
+    }
+
+    @Override
+    public void handleSetListening(boolean listening) {
+        if (mListening == listening)
+            return;
+        mListening = listening;
+        if (listening) {
+            mObserver.startObserving();
+        } else {
+            mObserver.endObserving();
+        }
+    }
+
+    @Override
+    protected void handleClick(@Nullable View view) {
+        changeToNextMode();
+    }
+
+    @Override
+    protected void handleUpdateState(LiveDisplayState state, Object arg) {
+        updateEntries();
+        state.mode = arg == null ? getCurrentModeIndex() : (Integer) arg;
+        state.label = mTitle;
+        state.secondaryLabel = mEntries[state.mode];
+        state.icon = ResourceIcon.get(mEntryIconRes[state.mode]);
+        state.contentDescription = mDescriptionEntries[state.mode];
+        state.state = mLiveDisplay.getMode() != MODE_OFF ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.CUSTOM;
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.live_display_title);
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return DISPLAY_SETTINGS;
+    }
+
+    private int getCurrentModeIndex() {
+        String currentLiveDisplayMode = null;
+        try {
+            currentLiveDisplayMode = String.valueOf(mLiveDisplay.getMode());
+        } catch (NullPointerException e) {
+            currentLiveDisplayMode = String.valueOf(MODE_AUTO);
+        } finally {
+            return ArrayUtils.indexOf(mValues, currentLiveDisplayMode);
+        }
+    }
+
+    private void changeToNextMode() {
+        int next = getCurrentModeIndex() + 1;
+
+        if (next >= mValues.length) {
+            next = 0;
+        }
+
+        int nextMode = 0;
+
+        while (true) {
+            nextMode = Integer.valueOf(mValues[next]);
+            // Skip outdoor mode if it's unsupported, skip the day setting
+            // if it's the same as the off setting, and skip night display
+            // on HWC2
+            if ((!mOutdoorModeAvailable && nextMode == MODE_OUTDOOR) ||
+                    (mDayTemperature == OFF_TEMPERATURE && nextMode == MODE_DAY) ||
+                    (mNightDisplayAvailable && (nextMode == MODE_DAY || nextMode == MODE_NIGHT))) {
+                next++;
+                if (next >= mValues.length) {
+                    next = 0;
+                }
+            } else {
+                break;
+            }
+        }
+
+        Settings.System.putIntForUser(mContext.getContentResolver(),
+                Settings.System.DISPLAY_TEMPERATURE_MODE, nextMode,
+                UserHandle.USER_CURRENT);
+    }
+
+    private class LiveDisplayObserver extends ContentObserver {
+        public LiveDisplayObserver(Handler handler) {
+            super(handler);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            mDayTemperature = mLiveDisplay.getDayColorTemperature();
+            refreshState(getCurrentModeIndex());
+        }
+
+        public void startObserving() {
+            mContext.getContentResolver().registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.DISPLAY_TEMPERATURE_MODE),
+                    false, this, UserHandle.USER_ALL);
+            mContext.getContentResolver().registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.DISPLAY_TEMPERATURE_DAY),
+                    false, this, UserHandle.USER_ALL);
+        }
+
+        public void endObserving() {
+            mContext.getContentResolver().unregisterContentObserver(this);
+        }
+    }
+}

xDroidOSS-Pixel/platform_frameworks_base

packages/SystemUI/src/com/android/systemui/qs/tiles/LiveDisplayTile.java
@@ -1,0 +1,251 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ * Copyright (C) 2018-2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import static com.android.internal.lineage.hardware.LiveDisplayManager.FEATURE_MANAGED_OUTDOOR_MODE;
+import static com.android.internal.lineage.hardware.LiveDisplayManager.MODE_AUTO;
+import static com.android.internal.lineage.hardware.LiveDisplayManager.MODE_DAY;
+import static com.android.internal.lineage.hardware.LiveDisplayManager.MODE_NIGHT;
+import static com.android.internal.lineage.hardware.LiveDisplayManager.MODE_OFF;
+import static com.android.internal.lineage.hardware.LiveDisplayManager.MODE_OUTDOOR;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.database.ContentObserver;
+import android.hardware.display.ColorDisplayManager;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.service.quicksettings.Tile;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+import com.android.internal.lineage.hardware.LiveDisplayManager;
+import com.android.internal.logging.MetricsLogger;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.util.ArrayUtils;
+import com.android.systemui.dagger.qualifiers.Background;
+import com.android.systemui.dagger.qualifiers.Main;
+import com.android.systemui.plugins.ActivityStarter;
+import com.android.systemui.plugins.FalsingManager;
+import com.android.systemui.plugins.qs.QSTile.LiveDisplayState;
+import com.android.systemui.plugins.statusbar.StatusBarStateController;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.qs.logging.QSLogger;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+
+import javax.inject.Inject;
+
+import com.android.internal.R;
+
+/**
+ * Quick settings tile: LiveDisplay mode switcher
+ **/
+public class LiveDisplayTile extends QSTileImpl<LiveDisplayState> {
+
+    private static final Intent DISPLAY_SETTINGS = new Intent("android.settings.DISPLAY_SETTINGS");
+
+    private final LiveDisplayObserver mObserver;
+    private String mTitle;
+    private String[] mEntries;
+    private String[] mDescriptionEntries;
+    private String[] mAnnouncementEntries;
+    private String[] mValues;
+    private final int[] mEntryIconRes;
+
+    private boolean mListening;
+
+    private int mDayTemperature;
+
+    private final boolean mNightDisplayAvailable;
+    private final boolean mOutdoorModeAvailable;
+
+    private final LiveDisplayManager mLiveDisplay;
+
+    private static final int OFF_TEMPERATURE = 6500;
+
+    @Inject
+    public LiveDisplayTile(
+            QSHost host,
+            @Background Looper backgroundLooper,
+            @Main Handler mainHandler,
+            FalsingManager falsingManager,
+            MetricsLogger metricsLogger,
+            StatusBarStateController statusBarStateController,
+            ActivityStarter activityStarter,
+            QSLogger qsLogger) {
+        super(host, backgroundLooper, mainHandler, falsingManager, metricsLogger,
+                statusBarStateController, activityStarter, qsLogger);
+        mNightDisplayAvailable = ColorDisplayManager.isNightDisplayAvailable(mContext);
+
+        Resources res = mContext.getResources();
+        TypedArray typedArray = res.obtainTypedArray(R.array.live_display_drawables);
+        mEntryIconRes = new int[typedArray.length()];
+        for (int i = 0; i < mEntryIconRes.length; i++) {
+            mEntryIconRes[i] = typedArray.getResourceId(i, 0);
+        }
+        typedArray.recycle();
+
+        updateEntries();
+
+        mLiveDisplay = LiveDisplayManager.getInstance(mContext);
+        if (mLiveDisplay.getConfig() != null) {
+            mOutdoorModeAvailable = mLiveDisplay.getConfig().hasFeature(MODE_OUTDOOR) &&
+                    !mLiveDisplay.getConfig().hasFeature(FEATURE_MANAGED_OUTDOOR_MODE);
+            mDayTemperature = mLiveDisplay.getDayColorTemperature();
+        } else {
+            mOutdoorModeAvailable = false;
+            mDayTemperature = -1;
+        }
+
+        mObserver = new LiveDisplayObserver(mHandler);
+        mObserver.startObserving();
+    }
+
+    private void updateEntries() {
+        Resources res = mContext.getResources();
+        mTitle = res.getString(R.string.live_display_title);
+        mEntries = res.getStringArray(R.array.live_display_entries);
+        mDescriptionEntries = res.getStringArray(R.array.live_display_description);
+        mAnnouncementEntries = res.getStringArray(R.array.live_display_announcement);
+        mValues = res.getStringArray(R.array.live_display_values);
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return !mNightDisplayAvailable || mOutdoorModeAvailable;
+    }
+
+    @Override
+    public LiveDisplayState newTileState() {
+        return new LiveDisplayState();
+    }
+
+    @Override
+    public void handleSetListening(boolean listening) {
+        if (mListening == listening)
+            return;
+        mListening = listening;
+        if (listening) {
+            mObserver.startObserving();
+        } else {
+            mObserver.endObserving();
+        }
+    }
+
+    @Override
+    protected void handleClick(@Nullable View view) {
+        changeToNextMode();
+    }
+
+    @Override
+    protected void handleUpdateState(LiveDisplayState state, Object arg) {
+        updateEntries();
+        state.mode = arg == null ? getCurrentModeIndex() : (Integer) arg;
+        state.label = mTitle;
+        state.secondaryLabel = mEntries[state.mode];
+        state.icon = ResourceIcon.get(mEntryIconRes[state.mode]);
+        state.contentDescription = mDescriptionEntries[state.mode];
+        state.state = mLiveDisplay.getMode() != MODE_OFF ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.XD_SETTINGS;
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.live_display_title);
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return DISPLAY_SETTINGS;
+    }
+
+    private int getCurrentModeIndex() {
+        String currentLiveDisplayMode = null;
+        try {
+            currentLiveDisplayMode = String.valueOf(mLiveDisplay.getMode());
+        } catch (NullPointerException e) {
+            currentLiveDisplayMode = String.valueOf(MODE_AUTO);
+        } finally {
+            return ArrayUtils.indexOf(mValues, currentLiveDisplayMode);
+        }
+    }
+
+    private void changeToNextMode() {
+        int next = getCurrentModeIndex() + 1;
+
+        if (next >= mValues.length) {
+            next = 0;
+        }
+
+        int nextMode = 0;
+
+        while (true) {
+            nextMode = Integer.valueOf(mValues[next]);
+            // Skip outdoor mode if it's unsupported, skip the day setting
+            // if it's the same as the off setting, and skip night display
+            // on HWC2
+            if ((!mOutdoorModeAvailable && nextMode == MODE_OUTDOOR) ||
+                    (mDayTemperature == OFF_TEMPERATURE && nextMode == MODE_DAY) ||
+                    (mNightDisplayAvailable && (nextMode == MODE_DAY || nextMode == MODE_NIGHT))) {
+                next++;
+                if (next >= mValues.length) {
+                    next = 0;
+                }
+            } else {
+                break;
+            }
+        }
+
+        Settings.System.putIntForUser(mContext.getContentResolver(),
+                Settings.System.DISPLAY_TEMPERATURE_MODE, nextMode,
+                UserHandle.USER_CURRENT);
+    }
+
+    private class LiveDisplayObserver extends ContentObserver {
+        public LiveDisplayObserver(Handler handler) {
+            super(handler);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            mDayTemperature = mLiveDisplay.getDayColorTemperature();
+            refreshState(getCurrentModeIndex());
+        }
+
+        public void startObserving() {
+            mContext.getContentResolver().registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.DISPLAY_TEMPERATURE_MODE),
+                    false, this, UserHandle.USER_ALL);
+            mContext.getContentResolver().registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.DISPLAY_TEMPERATURE_DAY),
+                    false, this, UserHandle.USER_ALL);
+        }
+
+        public void endObserving() {
+            mContext.getContentResolver().unregisterContentObserver(this);
+        }
+    }
+}